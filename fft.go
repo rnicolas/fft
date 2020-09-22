ad9d2948266e9ddc7256953e231d8225f375b220
Roger N. Alegret <roger.nicolas@aeinnova.com>
2020-09-22 11:58:04 +0200

    Added function to calculate the amplitude

--- fft.go
+++ fft.go
@@ -5,6 +5,7 @@ import (
 	"math/cmplx"
 )
 
+//FFT calculates the Fast Fourier Transform
 func FFT(x []complex128, n int) []complex128 {
 	y := fft(x, n)
 	for i := 0; i < n; i++ {
@@ -13,6 +14,7 @@ func FFT(x []complex128, n int) []complex128 {
 	return y
 }
 
+//IFFT calculates the Inverse Fast Fourier Transform
 func IFFT(x []complex128, n int) []complex128 {
 	y := make([]complex128, n)
 	for i := 0; i < n; i++ {
@@ -23,6 +25,15 @@ func IFFT(x []complex128, n int) []complex128 {
 		y[i] = cmplx.Conj(y[i])
 	}
 	return y
+}
+
+//GetAmplitude alculates frequencies amplitude given a FFT m
+func GetAmplitude(m []complex128) []float64 {
+	var a []float64
+	for index := 0; index < (len(m)/2)+1; index++ {
+		a[index] = math.Sqrt(math.Pow(real(m[index]), 2) + math.Pow(imag(m[index]), 2))
+	}
+	return a
 }
 
 func fft(a []complex128, n int) []complex128 {
@@ -36,10 +47,14 @@ func fft(a []complex128, n int) []complex128 {
 		}
 		m := n / 2
 		for {
-			if j < m { break }
+			if j < m {
+				break
+			}
 			j = j - m
 			m = m / 2
-			if m < 2 { break }
+			if m < 2 {
+				break
+			}
 		}
 		j = j + m
 	}
@@ -50,13 +65,13 @@ func fft(a []complex128, n int) []complex128 {
 		}
 		istep := kmax * 2
 		for k := 0; k < kmax; k++ {
-			theta := complex(0.0, -1.0 * math.Pi * float64(k) / float64(kmax))
+			theta := complex(0.0, -1.0*math.Pi*float64(k)/float64(kmax))
 			for i := k; i < n; i += istep {
 				j := i + kmax
 				temp := x[j] * cmplx.Exp(theta)
 				x[j] = x[i] - temp
 				x[i] = x[i] + temp
-			} 
+			}
 		}
 		kmax = istep
 	}
